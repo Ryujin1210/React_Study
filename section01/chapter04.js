7c9cbd0f8bbfb0e89b78e2794aae2b5e88b2c249
Ryu <ywg6525@daum.net>
2024-11-01 15:45:41 +0900

    first commit

---
+++ section01/chapter04.js
@@ -0,0 +1,28 @@
+// 1. 변수
+let age;
+// console.log(age);
+
+age = 30;
+// console.log(age);
+
+// 2. 상수
+const birth = '2000.12.10';
+
+// 3. 변수 명명규칙 (네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용할 수 없다.
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+let name1l;
+let $2name;
+
+// 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드
+let a = 1;
+let b = 1;
+let c = a - b;
+
+let salesCount = 1;
+let refundCotunt = 1;
+let totalSalesCount = salescount - refundcotunt;
