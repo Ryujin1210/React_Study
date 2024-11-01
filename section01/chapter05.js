96f5eabb64460adfc15ada4599ecb70787297f12
Ryu <ywg6525@daum.net>
2024-11-01 15:59:08 +0900

    자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,33 @@
+// 1. Number Type
+
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity; // 양의 무한대
+let mInf = -Infinity; // 음의 무한대
+
+let nan = NaN;
+
+// console.log(1 * 'hello'); // nan 연산 불가능
+
+// 2. String Type
+let myName = '유원근';
+let myLocation = '상계동';
+let introduce = myName + myLocation;
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
+
+// null은 명시적으로 할당 필요, undefined는 초기화 하지 않거나 자동적으로 할당된 것임.
