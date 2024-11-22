f030054c973dc71017ae7a44884b31519a3c48e9
Ryu <ywg6525@daum.net>
2024-11-22 10:48:02 +0900

    함수

---
+++ section01/chapter11.js
@@ -0,0 +1,44 @@
+// 함수 선언
+
+// function greeting() {
+//   console.log('안녕하세요.');
+// }
+
+// console.log('호출 전');
+// greeting();
+// console.log('호출 후');
+
+// 매개변수
+function getArea(width, height) {
+  function another() {
+    // 중첩 함수
+    console.log('another');
+  }
+
+  another();
+  let area = width * height;
+
+  return area;
+  console.log('리턴 후 실행안됌');
+}
+
+let area1 = getArea(120, 100);
+console.log(area1);
+
+// 10,20 인수
+getArea2(10, 20);
+
+// 호이스팅
+// -> 끌어올리다 라는 뜻 함수가 아래에 있어도 가능함
+function getArea2(width, height) {
+  function another() {
+    // 중첩 함수
+    console.log('another');
+  }
+
+  another();
+  let area = width * height;
+
+  return area;
+  console.log('리턴 후 실행안됌');
+}
